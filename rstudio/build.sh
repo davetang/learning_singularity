d707cf8e3ad79b0eaafc037d7e1a5a5fc8f76370
Dave Tang <davetingpongtang@gmail.com>
2024-02-02 08:33:39 +0900

    Include RStudio Server version in image file

--- rstudio/build.sh
+++ rstudio/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 R_VERSION=$(cat ./Singularity.def | grep R_VERSION= | cut -f2 -d'=')
-
 if [[ -z ${R_VERSION} ]]; then
    echo Could not get R version
    exit 1
 fi
 
-singularity build --fakeroot rstudio_server_${R_VERSION}.sif Singularity.def
+RSTUDIO_VERSION=$(basename $(cat ./Singularity.def | grep RSTUDIO_SERVER_URL= | cut -f2 -d'=') .deb)
+if [[ -z ${RSTUDIO_VERSION} ]]; then
+   echo Could not get RStudio Server version
+   exit 1
+fi
+
+singularity build --fakeroot ${RSTUDIO_VERSION}-R-${R_VERSION}.sif Singularity.def
