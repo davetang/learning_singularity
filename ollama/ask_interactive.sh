0899c71a7aacbf32280696cb11713f753e1bb689
Dave Tang <davetingpongtang@gmail.com>
2025-01-29 12:44:58 +0900

    Start interactive session

---
+++ ollama/ask_interactive.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+INSTANCE_NAME=ollama_instance
+
+# find models at https://ollama.com/library
+MODEL=deepseek-r1:32b
+
+FOUND=0
+if singularity instance list | grep -q ${INSTANCE_NAME}; then
+   FOUND=1
+fi
+
+if [[ ${FOUND} == 1 ]]; then
+   >&2 echo Found running instance: ${INSTANCE_NAME}
+else
+   >&2 echo Could not find ${INSTANCE_NAME}
+   >&2 echo Starting ${INSTANCE_NAME}
+   singularity instance start ollama.sif ${INSTANCE_NAME}
+   sleep 5
+fi
+
+singularity exec instance://${INSTANCE_NAME} ollama run ${MODEL}
